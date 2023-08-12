8d6b7a8d073d77be6638aaeafe0f39de79b7cdd1
Nishant Sourav <nishant.sharma8507966@gmail.com>
2023-08-12 11:53:55 +0530

    version1.2

--- first_app.js
+++ first_app.js
@@ -1,7 +1,28 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log('Nishant Sourav');
+const server = http.createServer((req,res)=> {
+    //res.setHeader('Content-Type','text/html');
+    if(req.url ==='/home') {
+        res.write('<html>');
+        res.write('<head><title>My Fist Page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(req.url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>My Second Page</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html');
+        res.end();
+    }
+    else if(req.url ==='/node') {
+        res.write('<html>');
+        res.write('<head><title>My Thrid Page</title></head>');
+        res.write('<body><h1>Welcome to My Node Js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
-server.listen(4000);
+server.listen(3000)
