5ec95804fc6266271ff97c680f62a2369402737c
Nishant Sourav <nishant.sharma8507966@gmail.com>
2023-08-13 12:02:11 +0530

    version1.6

--- first_app.js
+++ first_app.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Hey There!');
-    next();
-})
+app.use(bodyparser.urlencoded());
 
-app.use((req,res,next) => {
-    console.log('I am Fine');
-    res.send('<h1> hello to node js </h1>')
-})  
+app.use('/add-product', (req,res,next) => {
+    res.send(`<form action='/product' method='POST'>
+    <label for="product">Product Name:</label>
+    <input type="text" id="product" name="product"><br>
+    <label for="size">Product Size:</label>
+    <input type="text" id="size" name="size"><br>
+    <button type='submit'>Add product</button>
+    </form>`)
+}) ; 
+
+
+app.use('/product', (req,res,next)=> {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+app.use('/', (req,res,next)=> {
+    res.send('<h1>Hello from express</h1>');
+});
 
 app.listen(3000)
