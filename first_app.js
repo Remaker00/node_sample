da4e7553cab23dd1bcc0dc25cfd609895b6f39e2
Nishant Sourav <nishant.sharma8507966@gmail.com>
2023-08-13 15:08:59 +0530

    version1.7

--- first_app.js
+++ first_app.js
@@ -14,4 +14,5 @@ app.use((req,res,next)=>{
     res.status(404).send('<h1>Page not Found!</h1>');
 });
 
+
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,6 @@ router.get('/add-product', (req,res,next) => {
     </form>`)
 }) ; 
 
-
 router.post('/add-product', (req,res,next)=> {
     console.log(req.body);
     res.redirect('/');
